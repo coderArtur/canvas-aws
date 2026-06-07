aa885f77b98c5dd0f257a019b00d12506b684726
coderArtur <coderartur@gmail.com>
2026-06-07 14:13:53 -0300

    Finalizado

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+# pyrefly: ignore [missing-import]
+from dotenv import load_dotenv
+
+load_dotenv()
+
+OCULTAR_TELA = True
+EMAIL = os.getenv("EMAIL")
+SENHA = os.getenv("SENHA")
+URL_LOGIN = "https://awsrestart.instructure.com/login/canvas"
+URL_ATIVIDADES = "https://awsrestart.instructure.com/courses/4327/grades"
+BASE_URL = "https://awsrestart.instructure.com"
